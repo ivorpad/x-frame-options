2d164ea61144524d8cb708d12028745690429888
ivorpad <ivorjpc@gmail.com>
2024-01-10 13:03:14 +0100

    test

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   // Clone the request headers and set a new header `x-hello-from-middleware1`
   const requestHeaders = new Headers(request.headers);
-  requestHeaders.set("x-hello-from-middleware1", "hello");
+  requestHeaders.set("Content-Security-Policy", "frame-ancestors 'self' https://codepen.io");
 
   // You can also set request headers in NextResponse.rewrite
   const response = NextResponse.next({
