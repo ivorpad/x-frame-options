a559d801b669b1ba98c70d24bc666011c6f3f3ca
ivorpad <ivorjpc@gmail.com>
2024-01-10 13:20:03 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,39 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self' https://codepen.io",
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'ALLOW-FROM https://codepen.io',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "style-src 'self' 'unsafe-inline' https://codepen.io",
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-        ],
-      },
-    ]
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/(.*)',
+  //       headers: [
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value: "frame-ancestors 'self' https://codepen.io",
+  //         },
+  //         {
+  //           key: 'X-Frame-Options',
+  //           value: 'ALLOW-FROM https://codepen.io',
+  //         },
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value: "style-src 'self' 'unsafe-inline' https://codepen.io",
+  //         },
+  //         {
+  //           key: 'X-Content-Type-Options',
+  //           value: 'nosniff',
+  //         },
+  //         {
+  //           key: 'X-XSS-Protection',
+  //           value: '1; mode=block',
+  //         },
+  //         {
+  //           key: 'Referrer-Policy',
+  //           value: 'strict-origin-when-cross-origin',
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
 }
 
 module.exports = nextConfig
