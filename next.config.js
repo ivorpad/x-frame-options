67abbc8b0f819d19a59037b6ed0121a72bb0c94b
ivorpad <ivorjpc@gmail.com>
2024-01-10 13:27:57 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,39 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/(.*)',
-  //       headers: [
-  //         {
-  //           key: 'Content-Security-Policy',
-  //           value: "frame-ancestors 'self' https://codepen.io",
-  //         },
-  //         {
-  //           key: 'X-Frame-Options',
-  //           value: 'ALLOW-FROM https://codepen.io',
-  //         },
-  //         {
-  //           key: 'Content-Security-Policy',
-  //           value: "style-src 'self' 'unsafe-inline' https://codepen.io",
-  //         },
-  //         {
-  //           key: 'X-Content-Type-Options',
-  //           value: 'nosniff',
-  //         },
-  //         {
-  //           key: 'X-XSS-Protection',
-  //           value: '1; mode=block',
-  //         },
-  //         {
-  //           key: 'Referrer-Policy',
-  //           value: 'strict-origin-when-cross-origin',
-  //         },
-  //       ],
-  //     },
-  //   ]
-  // },
+// const ContentSecurityPolicy = `
+// default-src 'self';
+// script-src 'self' 'unsafe-eval' 'unsafe-inline';
+// child-src;
+// style-src 'self' 'unsafe-inline';
+// img-src * blob: data:;
+// media-src 'none';
+// connect-src *;
+// font-src 'self' data:;
+// frame-ancestors 'self' localhost:*;
+
+ headers: async () => {
+  return [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Content-Security-Policy',
+          value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; child-src; style-src 'self' 'unsafe-inline'; img-src * blob: data:; media-src 'none'; connect-src *; font-src 'self' data:; frame-ancestors 'self' https://codepen.io;",
+        },
+      ],
+    },
+  ]
+ }
+
 }
 
 module.exports = nextConfig
