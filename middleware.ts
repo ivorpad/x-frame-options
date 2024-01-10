098d3d7ac1853e3a4ed11c250341cc25ec3573a4
Ivor <ivorpad@users.noreply.github.com>
2024-01-10 13:45:08 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,15 +3,9 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   if (process.env.VERCEL_ENV === "preview") {
-    // Custom logic for preview deployments
     const response = NextResponse.next();
-    //  allow from codepen
     response.headers.set("X-Frame-Options", "SAMEORIGIN");
-    response.headers.set("Cross-Origin-Resource-Policy", "cross-origin");
-    response.headers.set("Content-Security-Policy", "frame-ancestors 'self' https://codepen.io");
-
     return response;
   }
-  // Default behavior for other environments
   return NextResponse.next();
 }
